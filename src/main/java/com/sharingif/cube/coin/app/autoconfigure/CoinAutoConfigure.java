31ce2e056ad49d5dee174b0b45009327d7e0f068
JolyHuang <JolyEmail@Gmail.COM>
2017-07-18 10:15:37 +0800

    优化

---
+++ src/main/java/com/sharingif/cube/coin/app/autoconfigure/CoinAutoConfigure.java
@@ -0,0 +1,16 @@
+package com.sharingif.cube.coin.app.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * CoinAutoConfigure
+ *
+ * @author Joly
+ * @version v1.0
+ * @since v1.0
+ * 2017/7/16 下午11:04
+ */
+@Configuration
+public class CoinAutoConfigure {
+
+}
